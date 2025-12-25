a878e13d035c4647e6c0545cd746dee3115e530b
Amine ER <erraysse.amine@gmail.com>
2025-12-25 16:59:35 +0100

    Fix errors and formatting

--- helpers/ts-finite.ts
+++ helpers/ts-finite.ts
@@ -1,23 +1,9 @@
-type Not<N, T, A = any> = [T] extends [N] ? never : (
-  [T] extends [A] ? T : never
-);
-
-type Infinity = 1e999 | -1e999;
-type LiteralNumber<T> = T extends number ? (
-  number extends T ? never : T
-) : never;
-type FiniteNumber<T> = Not<Infinity, T, LiteralNumber<T>>;
-
-/* 
-// In this use case `Not` is not really needed, you can just implement it directly with `LiteralNumber`:
+type Infinity = 1e309 | -1e309;
 type FiniteNumber<T> = T extends number ? (
   number extends T ? never : (
-    T extends Infinity ? never : (
-      T extends Infinity ? never : T
-    )
+    T extends Infinity ? never : T
   )
 ) : never;
- */
 
 const takesFiniteNumber = <T extends number>(n: FiniteNumber<T>) => n;
 
@@ -30,3 +16,23 @@ takesFiniteNumber(-1e999);
 takesFiniteNumber(Infinity);
 takesFiniteNumber(-Infinity);
 takesFiniteNumber(NaN);
+
+// We can also implement this using the `Not` type helper from ./ts-not.ts
+
+import { type Not } from "./ts-not";
+type LiteralNumber<T> = T extends number ? (
+  number extends T ? never : T
+) : never;
+type Finite<T> = Not<Infinity, T, LiteralNumber<T>>; // Same as `FiniteNumber`, but using `Not`
+
+const takesFinite = <T extends number>(n: Finite<T>) => n;
+
+takesFinite(10);
+takesFinite(3.14);
+takesFinite(-42.5);
+takesFinite(1e6);
+takesFinite(1e999);
+takesFinite(-1e999);
+takesFinite(Infinity);
+takesFinite(-Infinity);
+takesFinite(NaN);
