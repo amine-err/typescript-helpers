105de4c8afbf0a0b6768042f5804276a662d1423
Amine ER <erraysse.amine@gmail.com>
2025-12-25 06:05:00 +0100

    Remove dependency and reformat ts-finite.ts

--- helpers/ts-finite.ts
+++ helpers/ts-finite.ts
@@ -1,10 +1,23 @@
-import { Not } from "./ts-not";
+type Not<N, T, A = any> = [T] extends [N] ? never : (
+  [T] extends [A] ? T : never
+);
 
 type Infinity = 1e999 | -1e999;
 type LiteralNumber<T> = T extends number ? (
   number extends T ? never : T
 ) : never;
 type FiniteNumber<T> = Not<Infinity, T, LiteralNumber<T>>;
+
+/* 
+// In this use case `Not` is not really needed, you can just implement it directly with `LiteralNumber`:
+type FiniteNumber<T> = T extends number ? (
+  number extends T ? never : (
+    T extends Infinity ? never : (
+      T extends Infinity ? never : T
+    )
+  )
+) : never;
+ */
 
 const takesFiniteNumber = <T extends number>(n: FiniteNumber<T>) => n;
 
@@ -17,14 +30,3 @@ takesFiniteNumber(-1e999);
 takesFiniteNumber(Infinity);
 takesFiniteNumber(-Infinity);
 takesFiniteNumber(NaN);
-
-/* 
-// In this use case `Not` is not really needed, you can just implement it directly with `LiteralNumber`:
-type FiniteNumber<T> = T extends number ? (
-  number extends T ? never : (
-    T extends Infinity ? never : (
-      T extends Infinity ? never : T
-    )
-  )
-) : never;
- */
