e8167366f1d9d54cdfd44b600a09029a5492bd15
Amine ER <erraysse.amine@gmail.com>
2025-12-25 01:42:48 +0100

    Add comments to ts-xor.ts

--- helpers/ts-xor.ts
+++ helpers/ts-xor.ts
@@ -1,6 +1,16 @@
+/** Given type `T`, if it is an object, it will return a union of all keys of `T`. */
 type AllKeys<T> = T extends object ? keyof T : never;
+/** Given type `T`, it will return an object type of all properties of `T` as optional, and their values set to never. */
 type AllProps<T> = {[K in AllKeys<T>]?: never};
+/**
+ * Returns a new object type built from the combined properties of the given `T` and `U` types.\
+ * It removes from `U` properties that already exists in `T`.
+ */
 type Combine<T, U> = (T & Omit<U, keyof T>) extends infer V ? {[K in keyof V]: V[K]} : never;
+/**
+ * Returns a combined object type of `T` and `U`, where all properties of `U` are set to never.\
+ * If `T` is not an object, it returns `T` as is.
+ */
 type Only<T, U extends object> = T extends object ? Combine<T, U> : T;
 type XOR<T> = Only<T, AllProps<T>>;
 
